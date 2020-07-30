fabba6373ebcdd34960e39019c58adf2d3f517cf
Alexander Permyakov <pamugk@gmail.com>
2020-07-30 22:05:59 +0500

    Initialized solution structure

---
+++ Textures/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Textures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
